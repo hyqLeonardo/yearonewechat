fc6cb2489e4639fa409edeab9ac14e884c7988d9
hyqLeonardo <hyq335335@163.com>
2017-08-12 15:31:14 +0800

    print debug info in acquire

--- acquire.py
+++ acquire.py
@@ -8,12 +8,20 @@ class Acquire(object):
 
     def GET(self):
 
-        data = web.input()
-        if len(data) == 0:
-            return "hello, no data found"
+        try:
+            web_data = web.data()
+            print("Acquire's Get webdata is ", web_data)   # 后台打日志
+        except Exception as ex:
+            print(ex)
+
+        input_param = web.input()
+        if len(input_param) == 0:
+            print("no param in url")
+            return "no input parameters found"
 
         # file path is set in url's param file, e.g ?file=./file/event/today_str/xxx.html
-        html_file_path = data.file
+        html_file_path = input_param.file
+        print('file path: {}'.format(html_file_path))
 
         with open(html_file_path, 'r') as file:
             html = file.read()
